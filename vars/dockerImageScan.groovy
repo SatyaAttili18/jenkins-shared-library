be4da1fde3d7256db3d014b8a99f5d59a08bec30
SatyaAttili18 <111959661+SatyaAttili18@users.noreply.github.com>
2025-05-05 20:21:00 +0530

    Create dockerImageScan.groovy

---
+++ vars/dockerImageScan.groovy
@@ -0,0 +1,11 @@
+// vars/dockerImageScan.groovy
+def call(Map config) {
+    def image = config.image
+    def tag = config.tag
+
+    echo "🔍 Scanning Docker image ${image}:${tag} with Trivy..."
+
+    sh """
+        trivy image ${image}:${tag}
+    """
+}
